720762de5a2d15529c63c51b9218b1fb607f6cd4
caro-mugo <mugonycaroline@gmail.com>
2022-05-05 20:21:12 +0300

    first commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.helloworld
+
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+   lateinit var btnBmi: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnBmi = findViewById(R.id.btnBmi)
+        btnBmi.setOnClickListener {
+            val intent = Intent(this,BMIcalculatoractivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
